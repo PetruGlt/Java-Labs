5be5d377ba8c31a62779247e7b3da0010b8edd79
PetruGlt <galateanupetru152@gmail.com>
2025-03-21 10:11:45 +0200

    compulsory checked

--- Lab4/src/main/java/org/example/Location.java
+++ Lab4/src/main/java/org/example/Location.java
@@ -1,4 +1,39 @@
 package org.example;
 
-public class Location {
+public class Location implements Comparable<Location> {
+    enum Type {
+        FRIENDLY,
+        NEUTRAL,
+        ENEMY
+    }
+
+    private String name;
+    private Type type;
+
+    public Location(String name, Type type) {
+        this.name = name;
+        this.type = type;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public int compareTo(Location o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
---
+++ Lab4/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var locations = IntStream.rangeClosed(0, 4)
+                .mapToObj(i -> new Location("P" + i, Math.random()<0.5 ? Location.Type.FRIENDLY : Math.random()<0.5 ? Location.Type.NEUTRAL : Location.Type.ENEMY ) )
+                .toArray(Location[]::new);
+
+        for(Location location : locations) {
+            System.out.println(location);
+        }
+        System.out.println("----------------");
+        Set<Location> friendlyLocations = Arrays.stream(locations)
+                .filter(location -> location.getType() == Location.Type.FRIENDLY)
+                .sorted(Comparator.comparing(Location::getName))
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        friendlyLocations.forEach(System.out::println);
+        System.out.println("-------------------");
+
+        List<Location> enemyLocations = Arrays.stream(locations)
+                .filter(location -> location.getType() == Location.Type.ENEMY)
+                .sorted(Comparator.comparing(Location::getName).thenComparing(Location::getType))
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        enemyLocations.forEach(System.out::println);
+    }
+
+
+}
